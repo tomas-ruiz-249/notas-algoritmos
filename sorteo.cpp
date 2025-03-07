0c659fd4cdbd42d48cd76792832d8d0eb933ed37
tomas <tomas.ruizcorrea@gmail.com>
2025-03-07 07:24:26 -0500

    insert and selection sort

--- sorteo.cpp
+++ sorteo.cpp
@@ -30,17 +30,43 @@ void bubbleSort(int* arr, int size){
 }
 
 void insertionSort(int* arr, int size){
+    int temp;
+    int previous;
+    for(int i = 1; i < size; i++){
+        temp = arr[i];
+        previous = i - 1;
+        while(previous >= 0 && arr[previous] > temp){
+            arr[previous + 1] = arr[previous];
+            previous--;
+        }
+        arr[previous + 1] = temp;
+    }
+}
 
+void selectionSort(int* arr, int size){
+    int minIndex;
+    int temp;
+    for(int i = 0; i < size - 1; i++){
+        minIndex = i;
+        for(int j = i + 1; j < size; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        if(minIndex != i){
+            swap(arr[i], arr[minIndex]);
+        }
+    }
 }
 
 
 int main(){
     const int SIZE = 5;
-    int arr[SIZE] = {30,83,-4,-91,7};
+    int arr[SIZE] = {5,3,1,4,2};
     cout << "arreglo original:\n";
     printArray(arr, SIZE);
     cout << "---------------------\n";
-    bubbleSort(arr, SIZE);
+    selectionSort(arr, SIZE);
     printArray(arr, SIZE);
     return 0;
 }
