00bdda155dadbc1a349cb8ad564e4fdc0301e3b0
tomas <tomas.ruizcorrea@gmail.com>
2025-03-14 08:58:51 -0500

    linked list sort

--- linked_list.cpp
+++ linked_list.cpp
@@ -25,6 +25,9 @@ public:
     T getData(){
         return data;
     }
+    void setData(T data){
+        this->data = data;
+    }
     private:
     T data;
     Node<T>* next;
@@ -69,7 +72,7 @@ public:
     }
 
     T get(int index){
-        if(index > size || index < 0){
+        if(index >= size || index < 0){
             return T();
         }
         Node<T>* current = head;
@@ -77,6 +80,24 @@ public:
             current = current->getNext();
         }
         return current->getData();
+    }
+
+    Node<T>* getNode(int index){
+        if(index >= size || index < 0){
+            return nullptr;
+        }
+        if(index == size - 1){
+            return tail;
+        }
+        if(index == 0){
+            return head;
+        }
+        Node<T>* current = head;
+        for(int i = 0; i < index; i++){
+            current = current->getNext();
+        }
+        return current;
+
     }
 
     void append(T newValue){
@@ -151,7 +172,7 @@ public:
         size--;
     }
 
-    void remove(T value){
+    void removeValue(T value){
         int index = find(value);
         if(index == -1){
             cout << "could not find value to be removed\n";
@@ -191,8 +212,68 @@ public:
     Node<T>* getTail(){
         return tail;
     }
-private:
+
+    void sort(){
+        bubbleSort();
+    }
+
+private:    
+    void swap(int a, int b){
+        bool cantSwap = (a == b || a < 0 || a >= size || b < 0 || b >= size);
+        if(cantSwap){
+            return;
+        }
+        Node<T>* aPrev = head;
+        Node<T>* bPrev = head;
+        Node<T>* aNode = head;
+        Node<T>* bNode = head;
+        for(int i = 0; i < a - 1 ; i++){
+            aPrev = aPrev->getNext();
+        }
+        if(a != 0){
+            aNode = aPrev->getNext();
+        }
+
+        for(int i = 0; i < b - 1; i++){
+            bPrev = bPrev->getNext();
+        }
+        if(b != 0){
+            bNode = bPrev->getNext();
+        }
+
+        Node<T>* temp = bNode->getNext();
+        aPrev->setNext(bNode);
+        bNode->setNext(bPrev);
+        bPrev->setNext(aNode);
+        aNode->setNext(temp);
+    }
+
+    void bubbleSort(){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size - i - 1; j++){
+                if(get(j) > get(j + 1)){
+                    swap(j, j + 1);
+                }
+            }
+        }
+    }
+
     Node<T>* head;
     Node<T>* tail;
     int size;
 };
+
+int main(){
+    srand(time(0));
+    LinkedList<int> list;
+    int val;
+    for(int i = 10; i >= 0; i--){
+        // val = rand() % 10 + 1;
+        list.append(i);
+    }
+
+    list.print();
+    list.sort();
+    list.print();
+    return 0;
+}
